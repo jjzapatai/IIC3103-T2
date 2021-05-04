63fb818fa84543fff4619235783347b66565c4e2
JUAN ZAPATA <jjzapata1@uc.cl>
2021-05-04 14:21:46 -0400

    database

--- app.py
+++ app.py
@@ -73,7 +73,30 @@ class Track(db.Model):
     self.album = 'http://localhost:5000/albums/'+self.id
     self.self_url = 'http://localhost:5000/tracks/'+self.id
 
+# Artist Schema
+class ArtistSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'age', 'albums', 'tracks', 'self_url')
 
+# Album Schema
+class AlbumSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'artist_id', 'name', 'genre', 'artist', 'tracks', 'self_url')
+
+# Track Schema
+class TrackSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'album_id', 'name', 'duration', 'times_played', 'artist', 'album', 'self_url')
+
+#init schema
+artist_schema = ArtistSchema()
+artists_schema = ArtistSchema(many=True)
+
+album_schema = AlbumSchema()
+albums_schema = AlbumSchema(many=True)
+
+track_schema = TrackSchema()
+tracks_schema = TrackSchema(many=True)
 
 ## hacer python   ->  from app import db  -> db.create_all()  (y se crea la base de datos)
 ##########################################     ARTISTS     ##########################################     
